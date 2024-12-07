f5b3589c28b414a9e87a5d3dcc42c9e3ae70e8c4
Rodrigo Nahid <rn@tsl.io>
2024-12-07 18:53:58 -0300

    Fixing router

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from posts.rest_framework.viewsets import PostViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- posts/rest_framework/routers.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from posts.rest_framework.viewsets import PostViewSet
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='posts')
-# router.register(r'users', views.UserViewSet, basename='user')
