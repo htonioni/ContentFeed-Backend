5c277418d140ced972efa9da1c76fc060c85a10a
unknown <htonioni@outlook.com>
2024-12-08 15:43:53 -0300

    adding  user auth commit

--- posts/rest_framework/viewsets.py
+++ posts/rest_framework/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from posts.models import Post
 from posts.rest_framework.serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     """
@@ -8,4 +9,4 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # permission_classes = [IsAccountAdminOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
